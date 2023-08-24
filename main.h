85922f7f5c35e1352c11a91d70ada1d127ee96af
root <leratodubej@gmail.com>
2023-08-24 12:40:59 -0700

    main

--- main.h
+++ main.h
@@ -6,10 +6,9 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
+#include <fcntl.h>
 
 void execu(char **argv);
 char *get_location(char *command);
 int calcsum(int num1, int num2);
-
 #endif
